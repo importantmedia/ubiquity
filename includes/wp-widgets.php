3683eb10810502cb03e57fba1601f826366c6d9c
Jake Luer <jake.luer@gmail.com>
2010-08-10 16:46:22 -0700

    fbwidget standard backend form

--- includes/wp-widgets.php
+++ includes/wp-widgets.php
@@ -23,16 +23,12 @@ class UbiqFBStandard extends WP_Widget {
   /** @see WP_Widget::update */
   function update($new_instance, $old_instance) {				
     $instance = $old_instance;
-    $instance['title'] = strip_tags($new_instance['title']);
     return $instance;
   }
   
   /** @see WP_Widget::form */
   function form($instance) {				
-    $title = esc_attr($instance['title']);
-    ?>
-    <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
-    <?php 
+
   }
 
 } // class FooWidget
