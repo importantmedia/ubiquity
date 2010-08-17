037b9b439b500e48a6cb075dccc1d3f3a1a99130
Jake Luer <jake.luer@gmail.com>
2010-08-17 15:31:26 -0700

    twitter credentials

--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -7,7 +7,15 @@ if (0 < $numshares) $numshares = number_format($numshares);
 ?>
 
 <div class="ubiq_button_fb" href="<?php the_permalink() ?>"><div class="fbicon"></div><div class="sharetext">Share</div><div class="count"><?php echo $numshares ?></div></div>
+<?php
+if(get_the_author_meta('twitter_handle')) {
+  $data_related = get_the_author_meta('twitter_handle') . ":The author's account";
+} else {
+  $data_related = "importantmedia:This is the Important Media Network";
+}
 
-<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="Testing" data-count="horizontal" data-via="jakeluer" data-related="author:The Author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+?>
+
+<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title() ?>" data-count="horizontal" data-via="<?php get_option('ubiq_twtr_sitehandle') ?>" data-related="<?php echo $data_related ?>:The author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 
 
