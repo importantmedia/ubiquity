bf037d4a4d7801f79163769121bed3d07d6e0e33
Jake Luer <jake.luer@gmail.com>
2010-08-17 13:27:24 -0700

    social button changes

--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -1,2 +1,13 @@
-<div class="ubiq_button_fb" href="<?php the_permalink() ?>"><div class="fbicon"></div><div class="sharetext">Share</div><div class="count"></div></div>
-<fb:like href="<?php the_permalink() ?>" layout="button_count" show_faces="false"></fb:like>
+<?php
+$shareUrl = urlencode(get_permalink($post->ID));
+$shareTitle = urlencode($post->post_title);
+$fbLinkStats = simplexml_load_file('http://api.facebook.com/restserver.php?method=links.getStats&urls='.$shareUrl);
+$numshares = $fbLinkStats->link_stat->total_count;
+if (0 < $numshares) $numshares = number_format($numshares);
+?>
+
+<div class="ubiq_button_fb" href="<?php the_permalink() ?>"><div class="fbicon"></div><div class="sharetext">Share</div><div class="count"><?php echo $numshares ?></div></div>
+
+<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="Testing" data-count="horizontal" data-via="jakeluer" data-related="author:The Author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+
+
