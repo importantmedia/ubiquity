4a7b75f12f6f9bb8ce898eaade081ca7ac2d136d
Jake Luer <jake.luer@gmail.com>
2010-08-18 22:02:12 -0700

    decommision the share button

--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -15,6 +15,5 @@ if(get_the_author_meta('twitter_handle')) {
 ?>
 <ul class="ubiq_social_buttons">
   <li class="twtr"><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title() ?>" data-count="horizontal" data-via="<?php echo get_option('ubiq_twtr_sitehandle') ?>" data-related="<?php echo $data_related ?>:The author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
-  <li class="fbshr"><fb:share-button href="<?php the_permalink() ?>" type="button_count"></fb:share-button></li>
   <li class="fblikesml"><fb:like href="<?php the_permalink() ?>" layout="button_count" show_faces="false" font="lucida grande"></fb:like></li>
 </ul>
