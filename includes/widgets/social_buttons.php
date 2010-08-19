eb9d0a60dff2a687eaf2b2944516e928115db1ab
Jake Luer <jake.luer@gmail.com>
2010-08-18 22:03:24 -0700

    removing remnants of fbshare

--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -1,11 +1,4 @@
 <?php
-$shareUrl = urlencode(get_permalink($post->ID));
-$shareTitle = urlencode($post->post_title);
-$fbLinkStats = simplexml_load_file('http://api.facebook.com/restserver.php?method=links.getStats&urls='.$shareUrl);
-$numshares = $fbLinkStats->link_stat->total_count;
-if (0 < $numshares) $numshares = number_format($numshares);
-?>
-<?php
 if(get_the_author_meta('twitter_handle')) {
   $data_related = get_the_author_meta('twitter_handle') . ":The author's account";
 } else {
