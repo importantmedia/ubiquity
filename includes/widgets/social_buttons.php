541188aeb67a776a2c911a7b773db07a4c68e374
Jake Luer <jake.luer@gmail.com>
2010-08-18 21:36:15 -0700

    small changes to fb widgets

--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -5,8 +5,6 @@ $fbLinkStats = simplexml_load_file('http://api.facebook.com/restserver.php?metho
 $numshares = $fbLinkStats->link_stat->total_count;
 if (0 < $numshares) $numshares = number_format($numshares);
 ?>
-
-<div class="ubiq_button_fb" href="<?php the_permalink() ?>"><div class="fbicon"></div><div class="sharetext">Share</div><div class="count"><?php echo $numshares ?></div></div>
 <?php
 if(get_the_author_meta('twitter_handle')) {
   $data_related = get_the_author_meta('twitter_handle') . ":The author's account";
@@ -15,7 +13,8 @@ if(get_the_author_meta('twitter_handle')) {
 }
 
 ?>
-
-<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title() ?>" data-count="horizontal" data-via="<?php echo get_option('ubiq_twtr_sitehandle') ?>" data-related="<?php echo $data_related ?>:The author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-
-
+<ul class="ubiq_social_buttons">
+  <li class="twtr"><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title() ?>" data-count="horizontal" data-via="<?php echo get_option('ubiq_twtr_sitehandle') ?>" data-related="<?php echo $data_related ?>:The author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
+  <li class="fbshr"><fb:share-button href="<?php the_permalink() ?>" type="button_count"></fb:share-button></li>
+  <li class="fblikesml"><fb:like href="<?php the_permalink() ?>" layout="button_count" show_faces="false" font="lucida grande"></fb:like></li>
+</ul>
