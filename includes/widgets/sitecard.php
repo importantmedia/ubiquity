527e54d6480ab0e06a1eba289b6680034e1c0685
Jake Luer <jake.luer@gmail.com>
2010-10-14 19:43:36 -0700

    no border fb iframe!

--- includes/widgets/sitecard.php
+++ includes/widgets/sitecard.php
@@ -1,7 +1,7 @@
 <div id="ubiq_sitecard" class="clearfix">
   <?php if(get_option('ubiq_fb_fanpageurl')) { ?>
   <div id="facebook_like">
-    <iframe src="http://www.facebook.com/plugins/likebox.php?href=<?php echo urlencode(get_option('ubiq_fb_fanpageurl')) ?>&amp;width=195&amp;colorscheme=light&amp;connections=0&amp;stream=true&amp;header=true&amp;height=85" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:195px; height:85px;" allowTransparency="true"></iframe>
+    <iframe src="http://www.facebook.com/plugins/likebox.php?href=<?php echo urlencode(get_option('ubiq_fb_fanpageurl')) ?>&amp;width=195&amp;colorscheme=light&amp;connections=0&amp;stream=false&amp;header=false&amp;height=85" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:195px; height:85px;" allowTransparency="true"></iframe>
   </div>
   <?php } ?>
   <div id="social_buttons">
