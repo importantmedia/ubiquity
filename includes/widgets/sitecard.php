0ca4787a922420acafce3b043bebf3829e6f36dc
Jake Luer <jake.luer@gmail.com>
2010-10-14 19:38:39 -0700

    more fb iframe changes

--- includes/widgets/sitecard.php
+++ includes/widgets/sitecard.php
@@ -1,7 +1,7 @@
 <div id="ubiq_sitecard" class="clearfix">
   <?php if(get_option('ubiq_fb_fanpageid')) { ?>
   <div id="facebook_like">
-    <fb:like-box profile_id="<?php echo get_option('ubiq_fb_fanpageid') ?>" stream="false" header="true" connections="0" width="195" height="85"></fb:like-box>
+    <iframe src="http://www.facebook.com/plugins/likebox.php?href=<?php echo urlencode(get_option('ubiq_fb_fanpageurl')) ?>&amp;width=195&amp;colorscheme=light&amp;connections=0&amp;stream=true&amp;header=true&amp;height=85" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:195px; height:85px;" allowTransparency="true"></iframe>
   </div>
   <?php } ?>
   <div id="social_buttons">
--- includes/widgets/social_buttons.php
+++ includes/widgets/social_buttons.php
@@ -8,5 +8,5 @@ if(get_the_author_meta('twitter_handle')) {
 ?>
 <ul class="ubiq_social_buttons">
   <li class="twtr"><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title() ?>" data-count="horizontal" data-via="<?php echo get_option('ubiq_twtr_sitehandle') ?>" data-related="<?php echo $data_related ?>:The author's account.">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
-  <li class="fblikesml"><iframe src="http://www.facebook.com/plugins/like.php?href=<?php urlencode(the_permalink()) ?>&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></li>
+  <li class="fblikesml"><iframe src="http://www.facebook.com/plugins/like.php?href=<?php urlencode(the_permalink()) ?>&amp;layout=button_count&amp;show_faces=true&amp;width=80&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></li>
 </ul>
